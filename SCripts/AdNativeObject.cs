e584f80cc2df88b4dbf62372379be2da9e9fbb50
nhathuy7996 <nhat.huy.7996@gmail.com>
2023-01-17 18:06:31 +0700

    ad native object

--- SCripts/AdNativeObject.cs
+++ SCripts/AdNativeObject.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AdNativeObject : MonoBehaviour
+namespace HuynnLib
 {
-    public GameObject textConnect;
-    public RawImage adIcon, adChoice, adBG;
-    public Text callToAction, advertiser, headLine;
-    // Start is called before the first frame update
-    
+    public class AdNativeObject : MonoBehaviour
+    {
+        public GameObject textConnect;
+        public RawImage adIcon, adChoice, adBG;
+        public Text callToAction, advertiser, headLine;
+        // Start is called before the first frame update
+
+    }
 }
