5b4e1ed56b666eb657ed1ee9490af623ebd64ce9
nhathuy7996 <nhat.huy.7996@gmail.com>
2023-01-24 00:26:51 +0700

    test auto push

---
+++ Commands/push_git_cmd.sh
@@ -0,0 +1,3 @@
+﻿#!/bin/sh
+
+git add -A
--- SCripts/Editor/BuildDone.cs
+++ SCripts/Editor/BuildDone.cs
@@ -11,6 +11,9 @@ using HuynnLib;
 using System.IO;
 using Facebook.Unity.Settings;
 using UnityEngine.Networking.Types;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
 
 public class BuildDone : IPostprocessBuildWithReport
 {
@@ -25,6 +28,64 @@ public class BuildDone : IPostprocessBuildWithReport
 
             writer.Flush();
             writer.Close();
+        }
+
+        if (!EditorUserBuildSettings.buildAppBundle)
+        {
+            string cmdPath = FindCommand();
+            if (string.IsNullOrEmpty(cmdPath))
+                return;
+            ProcessStartInfo startInfo = new ProcessStartInfo()
+            {
+                FileName = cmdPath,
+                Arguments = string.Format("{0}_{1}/{2}_{3}:{4}",
+                report.summary.outputPath, DateTime.Now.Date, DateTime.Now.Month,DateTime.Now.Hour,DateTime.Now.Minute)
+            };
+
+            startInfo.UseShellExecute = true;
+            Process proc = new Process()
+            {
+                StartInfo = startInfo,
+            };
+            
+            proc.Start();
+
+        }
+    }
+
+    static string FindCommand()
+    {
+        string[] files = Directory.GetFiles(Application.dataPath, "*push_git_cmd.sh", SearchOption.AllDirectories).ToArray();
+        if (files.Length == 1)
+        {
+            return files[0];
+        }
+
+        Debug.LogError("==>Project dont have require .sh file. Can't auto push git!!!!!<==");
+        return null;
+    }
+
+    [MenuItem("3rdLib/Test")]
+    static void TestRunbash()
+    {
+        if (!EditorUserBuildSettings.buildAppBundle)
+        {
+            string cmdPath = FindCommand();
+            if (string.IsNullOrEmpty(cmdPath))
+                return;
+ 
+            System.Diagnostics.Process uploadProc = new System.Diagnostics.Process
+            {
+                StartInfo = {
+                    FileName = @"/System/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal",
+                    Arguments = cmdPath,
+                    UseShellExecute = false,
+                    CreateNoWindow = false,
+                    WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal
+                }
+            };
+
+            uploadProc.Start();
         }
     }
 
