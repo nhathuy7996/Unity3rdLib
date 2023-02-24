4e0676a2e57609371ba216106e3a29be4ab07a40
nhathuy7996 <nhat.huy.7996@gmail.com>
2023-02-24 14:11:11 +0700

    add some summary

--- Scripts/Manager/LoadingManager.cs
+++ Scripts/Manager/LoadingManager.cs
@@ -37,8 +37,13 @@ namespace DVAH
 
         float _loadingMaxvalue;
 
-        // Start is called before the first frame update
-
+        /// <summary>
+        /// Start Loading bar with an action call back
+        /// action will trigger when loading done (time out or done on condition)
+        /// action get a list<bool> stand for list of condition done or not
+        /// </summary>
+        /// <param name="onDone">callback when all condition done or timeout</param>
+        /// <returns> LoadingManager component</returns>
         public LoadingManager Init(Action<List<bool>> onDone = null)
         {
             
@@ -47,7 +52,6 @@ namespace DVAH
             {
                 _conditionDone.Add(false);
             }
-
 
             _loadingMaxvalue = _loading.maxValue;
             _loading.value = 0;
@@ -88,15 +92,27 @@ namespace DVAH
             return this;
         }
 
+        /// <summary>
+        /// You can set number of condition fight before start loading
+        /// </summary>
+        /// <param name="numberCondition">number of condition which loading will wait all done then stop (in amout of time)</param>
+        /// <param name="onDone">callback when all condition done or timeout</param>
+        /// <returns></returns>
+        public LoadingManager Init(int numberCondition, Action<List<bool>> onDone = null)
+        {
+            _numberCondition = numberCondition;
+            return Init(onDone);
+        }
         // Update is called once per frame
         void Update()
         {
             _loading.value += _loadingMaxvalue * Time.deltaTime / _maxTimeLoading;
-            _loadingText.text = string.Format("{0:0.0}%", _loading.value);
-
-
+            _loadingText.text = string.Format("{0:0.0}%", _loading.value); 
         }
 
+        /// <summary>
+        /// Stop loading immediately
+        /// </summary>
         public void StopLoading()
         { 
             if (_maxTimeLoading == 0.2f)
@@ -107,6 +123,11 @@ namespace DVAH
 
         }
 
+        /// <summary>
+        /// add callback when loading done
+        /// </summary>
+        /// <param name="callback">callback invoke when loading done</param>
+        /// <returns></returns>
         public LoadingManager AddOnDoneLoading(Action<List<bool>> callback)
         {
             if (_loading.value >= 99f)
@@ -118,6 +139,12 @@ namespace DVAH
             return this;
         }
 
+        /// <summary>
+        /// Using this to set a condition is done. If all condition is done,
+        /// then loading will stop immediately
+        /// </summary>
+        /// <param name="id">ID of condition you wanna mark done</param>
+        /// <returns></returns>
         public LoadingManager DoneCondition(int id)
         {
             if (id >= _conditionDone.Count)
@@ -145,7 +172,7 @@ namespace DVAH
         private void OnDrawGizmosSelected()
         {
      
-            if (_isUseLoading && _loading.value < 100)
+            if (_isUseLoading && !Application.isPlaying)
             {
                 _loadingPopUp.SetActive(true);
             }
