adf0d347de52b7fed42602cba6b5cd8d51f32b2d
nhathuy7996 <nhat.huy.7996@gmail.com>
2023-08-16 14:37:20 +0700

    add mrects

--- Scripts/Editor/MenuEditor.cs
+++ Scripts/Editor/MenuEditor.cs
@@ -174,13 +174,14 @@ public class MenuEditor
             "cd ../../\n" +
             "cd " + Application.dataPath + "\n" +
             "git add -A\n" +
-            "git commit -m \"release_" + PlayerSettings.bundleVersion + "\"\n" +
+            $"git commit -m \"release_{ PlayerSettings.bundleVersion}_{PlayerSettings.Android.bundleVersionCode} \"\n" +
             "git push origin HEAD:production_doNotCreateBranchFromHere -f";
         }
         else
         {
+            
             cmdLines = "/C git add -A&" +
-            "git commit -m \"release _" + PlayerSettings.bundleVersion + "\"&" +
+            $"git commit -m \"release_{ PlayerSettings.bundleVersion}_{PlayerSettings.Android.bundleVersionCode} \"&" +
             "git push origin HEAD:production_doNotCreateBranchFromHere -f";
         }
 
